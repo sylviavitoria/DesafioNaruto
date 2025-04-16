befed2e0cb8dec267a307930ffebbdf2933ff535
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-04-16 14:20:12 -0300

    chore: estrutura inicial do projeto

---
+++ src/test/java/com/sylviavitoria/naruto/NarutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sylviavitoria.naruto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NarutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
